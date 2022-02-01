70c152a195ec91fe155e572006a1a4492b77e845
Ma3tin <martin.mares@educanet.cz>
2022-02-01 13:11:24 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = () => {
+    fetch("https://gist.githubusercontent.com/saniyusuf/406b843afdfb9c6a86e25753fe2761f4/raw/523c324c7fcc36efab8224f9ebb7556c09b69a14/Film.JSON")
+        .then((response) => response.json())
+        .then((petr) => {
+            for (let i = 0; i < petr.length; i++) {
+                
+            }
+        });
+};
